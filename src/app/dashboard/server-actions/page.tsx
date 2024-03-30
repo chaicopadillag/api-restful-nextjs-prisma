0cd94b9a9dfdcb745d65ea67b043f5f7790b3f4b
Dev Chaico <chaicopadillag@gmail.com>
2024-03-30 16:48:49 -0500

    revalidate data

--- src/app/dashboard/server-actions/page.tsx
+++ src/app/dashboard/server-actions/page.tsx
@@ -1,8 +1,11 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import { CreateTodo, TodoGridActionServer } from "@/components";
 import prisma from "@/lib/prisma";
 
 export default async function ServerActionsPage() {
-    const todos = await prisma.todo.findMany({ orderBy: { updatedAt: 'desc' } })
+    const todos = await prisma.todo.findMany()
     return (
         <div>
             <h1 className="text-2xl">Lista de tareas (Server Todos)</h1>
--- src/app/dashboard/todos/page.tsx
+++ src/app/dashboard/todos/page.tsx
@@ -1,9 +1,12 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import { CreateTodo, TodoGrid } from "@/components";
 import prisma from "@/lib/prisma";
 
 export default async function RestTodoPage() {
 
-    const todos = await prisma.todo.findMany({ orderBy: { updatedAt: 'desc' } })
+    const todos = await prisma.todo.findMany()
     return (
         <div>
             <h1 className="text-2xl">Lista de tareas</h1>
